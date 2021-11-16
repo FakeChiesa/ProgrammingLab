f8c63bdc9099f8caf6ef9826cfbaeefc868891d0
FakeChiesa <93199522+FakeChiesa@users.noreply.github.com>
2021-11-16 16:44:03 +0000

    ho messo il sum

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print('Hello World')
+print('variabile 1:{}' .format(10))
+
+#commenti
+my_var=1 #variabile intera. se voglio float, stringhe boolean basta scrivere quello che mi pare (per le stringhe uso apici/virgolette)
+#slicio stringhe mia_stringa [0:50] dal primo al 50mo carattere
+# nan è numero non definito
+#not and e or li scrivo così normalmente
+#liste sono array ma puoi metterci quel che ti pare dentro esempio my_list = [1, 2] 
+#dizionario associa a chiavi dei valori
---
+++ lezione2.py
@@ -0,0 +1,3 @@
+my_list = [ 10, 20, 25]
+risultato= sum(my_list)
+print (risultato)
