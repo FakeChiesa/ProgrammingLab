d7125f79945fefe90a6c6deb6a409dabd0b58650
FakeChiesa <93199522+FakeChiesa@users.noreply.github.com>
2021-11-16 16:55:20 +0000

    in teoria a posto

--- lezione2.py
+++ lezione2.py
@@ -1,6 +1,5 @@
-my_list = [ 10, 20, 25]
-risultato=0
-for i in range (3):
-    risultato = risultato + my_list[i]
-
-print (risultato)
+my_list = [10, 20, 25] #la lista di numeri
+risultato=0 #inizializzo variabile
+for item in my_list: #per tutti gli item nella lista faccio
+    risultato = risultato + item
+print (risultato) #stampo a grafica il risultato
