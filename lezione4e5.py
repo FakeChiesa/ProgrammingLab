7bd723b21327023155f5c2060f2fcfa8acb8c898
FakeChiesa <93199522+FakeChiesa@users.noreply.github.com>
2021-11-30 15:54:07 +0000

    fatto

---
+++ lezione4e5.py
@@ -0,0 +1,46 @@
+class CSVFile():
+    
+    def __init__(self, name):
+        # Programmazione dell'attributo "name"
+        self.name = name
+    
+    def get_data(self, elenco):
+    # Istanziamento della Lista (è una lista di lista!)
+        try:
+            file = open("shampo_sales.csv", "r")
+        except:
+            print('mona il file non esiste')
+            file = open("shampoo_sales.csv", "r")
+    # Lettura del file, linea per linea
+        listoflist = []
+        for line in file:
+    # Istanziamento di elementi (che andranno nella lista)
+            elements = line
+            elements=elements.split(',')
+        # Salvataggio dei termini (esclusa l'intestazione) del file negli elementi
+            if elements[0] != 'Date':
+                lista=[]
+                date = elements[0]
+                value = float(elements[1])
+                # Aggiunta degli elementi alla lista con split di ogni riga su ","
+                lista.append(date)
+                lista.append(value)
+                listoflist.append(lista)
+    # Chiusura del file
+        file.close()
+        return (listoflist)
+        
+
+
+
+# --- PROGRAMMA PRINCIPALE ---
+my_file = CSVFile('shampoo_sales.csv')
+print(my_file)
+print('Nome del file: {}'.format(my_file.name))
+
+# Programmazione di una lista di un file
+# Istanziamento di una lista per salvare i valori
+list_1 = []
+# Apertura del file
+a=my_file.get_data(list_1)
+print(a)
