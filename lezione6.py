640b69b0672d1e0eea2fbbcefc1acd66b7c6c78e
FakeChiesa <93199522+FakeChiesa@users.noreply.github.com>
2021-12-02 17:35:31 +0000

    nuovo esercizio lezione 6

--- lezione6.py
+++ lezione6.py
@@ -3,18 +3,24 @@ class CSVFile():
     def __init__(self, name):
         # Programmazione dell'attributo "name"
         self.name = name
+
+        if type(name) == str:
+            print ('è una stringa')
     
-    def get_data(self, elenco):
+    def get_data(self, elenco, start = None, end = None):
+
     # Istanziamento della Lista (è una lista di lista!)
-        try:
-            file = open("shampo_sales.csv", "r")
-        except:
-            print('mona il file non esiste, apro quello giusto')
-            file = open("shampoo_sales.csv", "r")
-        # Lettura del file, linea per linea
+        file = open("shampoo_sales.csv", "r")
+        if (start>end):#controllo alcune caratteristiche dello start e dell' end 
+            raise Exception('Il valore di partenza non può essere MAGGIORE di quello di arrivo')
+            #tiro su l'errore e lo printo per dare chiarezza al Programmazione
+            #ripeto il passaggio con i vari errori poccibili immessi da tastiera 
+        if (len(self.name)<end):
+            raise Exception('il file ha più righe di quelle che si intendono stampare')
+    # Lettura del file, linea per linea
         listoflist = []
         for line in file:
-        # Istanziamento di elementi (che andranno nella lista)
+    # Istanziamento di elementi (che andranno nella lista)
             elements = line
             elements=elements.split(',')
         # Salvataggio dei termini (esclusa l'intestazione) del file negli elementi
@@ -29,19 +35,19 @@ class CSVFile():
                 listoflist.append(lista)
     # Chiusura del file
         file.close()
+        listoflist = listoflist[start : end]
         return (listoflist)
-        
-
 
 
 # --- PROGRAMMA PRINCIPALE ---
 my_file = CSVFile('shampoo_sales.csv')
 print(my_file)
 print('Nome del file: {}'.format(my_file.name))
-
+inizio=1
+fine=20
 # Programmazione di una lista di un file
 # Istanziamento di una lista per salvare i valori
 list_1 = []
 # Apertura del file
-a=my_file.get_data(list_1)
+a=my_file.get_data(list_1, inizio, fine)
 print(a)
